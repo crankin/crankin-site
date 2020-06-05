5943a50973efe0f9830c1781e03c0582d8e1429e
crankin <crankin@getcrank.in>
2020-06-05 14:42:25 -0500

    switch focus to blog, move css to grid

--- .eleventy.js
+++ .eleventy.js
@@ -1,18 +1,21 @@
+const pluginRss = require("@11ty/eleventy-plugin-rss");
+
 module.exports = function(eleventyConfig) {
   // Aliases are in relation to the _includes folder
   // eleventyConfig.addLayoutAlias('newproject', 'layouts/newproject.html');
 
+  eleventyConfig.addPlugin(pluginRss);
+
   eleventyConfig.addPassthroughCopy('css');
   eleventyConfig.addPassthroughCopy('admin');
   eleventyConfig.addPassthroughCopy('thumbs');
-  eleventyConfig.addPassthroughCopy('images');
+  // eleventyConfig.addPassthroughCopy('images');
 
   // Get the first `n` elements of a collection.
   eleventyConfig.addFilter("head", (array, n) => {
     if( n < 0 ) {
       return array.slice(n);
     }
-
     return array.slice(0, n);
   });
 
